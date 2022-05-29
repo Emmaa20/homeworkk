cd98030cd935611d1268222e374877737b4abbe8
Elmira5555555 <elmirakerimovah@gmail.com>
2022-05-29 01:51:11 -0700

    added DemoApp3

---
+++ DemoApp3/DemoApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+int adidassayi = 4;
+int guccisayi = 2;
+int nikesayi = 1;
+int adidasqiymeti = 200;
+int gucciqiymeti = 1000;
+int nikeqiymeti = 150;
+
+
+Console.WriteLine("FIRMA:");
+Console.WriteLine("Adidas");
+Console.WriteLine("Ayaqqabi sayi:");
+Console.WriteLine(adidassayi);
+Console.WriteLine("Birinin qiymeti ($)");
+Console.WriteLine(adidasqiymeti);
+Console.WriteLine("Hamısının birlikdə qiyməti ($) :");
+Console.WriteLine(adidassayi * adidasqiymeti);
+
+Console.WriteLine("FIRMA:");
+Console.WriteLine("Gucci");
+Console.WriteLine("Ayaqqabi sayi:");
+Console.WriteLine(guccisayi);
+Console.WriteLine("Birinin qiymeti ($)");
+Console.WriteLine(gucciqiymeti);
+Console.WriteLine("Hamısının birlikdə qiyməti ($) :");
+Console.WriteLine(guccisayi * gucciqiymeti);
+
+Console.WriteLine("FIRMA:");
+Console.WriteLine("Nike");
+Console.WriteLine("Ayaqqabi sayi:");
+Console.WriteLine(nikesayi);
+Console.WriteLine("Birinin qiymeti ($)");
+Console.WriteLine(nikeqiymeti);
+Console.WriteLine("Hamısının birlikdə qiyməti ($) :");
+Console.WriteLine(nikesayi * nikeqiymeti);
+
+Console.WriteLine("Yekun xerclenen mebleg:");
+Console.WriteLine(adidasqiymeti * adidassayi + gucciqiymeti * guccisayi + nikeqiymeti * nikesayi);
