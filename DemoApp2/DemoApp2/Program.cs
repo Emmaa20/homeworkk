d60c35b801d0374a631be2e29d35504f56d56c0e
Elmira5555555 <elmirakerimovah@gmail.com>
2022-05-29 01:49:39 -0700

    added DemoApp2

---
+++ DemoApp2/DemoApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+long x = 20000000000;
+double mudafie = 0.2 * x;
+double texnologiya = x * 0.1;
+double qalanbudce = x * 0.7;
+Console.WriteLine("Budce:");
+Console.WriteLine(x);
+Console.WriteLine("Mudafie Nazirliyine:");
+Console.WriteLine(mudafie);
+Console.WriteLine("Yuksek Texnologiya Nazirliyine:");
+Console.WriteLine(texnologiya);
+Console.WriteLine("Yerde Qalan Budce:");
+Console.WriteLine(qalanbudce);
